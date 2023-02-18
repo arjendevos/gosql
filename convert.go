c97d48ec6a6573969db799a0acbf2c5177eea273
Arjen de Vos <arjendevoz@gmail.com>
2023-02-18 20:01:25 +0100

    fix

---
+++ convert.go
@@ -0,0 +1,19 @@
+package gosql
+
+import "github.com/arjendevos/gosql/functions"
+
+type GoSQLConfig struct {
+	SchemeDir           string
+	MigrationDir        string
+	ModelOutputDir      string
+	ControllerOutputDir string
+}
+
+func Convert(c *GoSQLConfig) {
+	functions.Convert(&functions.GoSQLConfig{
+		SchemeDir:           c.SchemeDir,
+		MigrationDir:        c.MigrationDir,
+		ModelOutputDir:      c.ModelOutputDir,
+		ControllerOutputDir: c.ControllerOutputDir,
+	})
+}
